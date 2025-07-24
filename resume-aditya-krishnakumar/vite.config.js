f8a1a2e640cc6b3623aa5c5eefe1f9cdb1d44dfc
aditya_kk <adityakrishnakumar12@gmail.com>
2025-07-24 22:56:35 +0530

    changes to vite config again

--- resume-aditya-krishnakumar/vite.config.js
+++ resume-aditya-krishnakumar/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/resume-Aditya-Krishnakumar/',
+  base: '/resume-aditya-krishnakumar/',
   plugins: [react(), tailwindcss()],
 })
